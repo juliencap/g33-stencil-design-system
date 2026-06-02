f80db0d2c1b6f0057358d0e9a804a9f70ef93570
Julien Cap <4956238+juliencap@users.noreply.github.com>
2026-06-02 22:06:19 +0200

    g33-tokens.css

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'girondins33-design-system',
+  globalStyle: 'src/global/global.css',
   outputTargets: [
     {
       type: 'dist',
