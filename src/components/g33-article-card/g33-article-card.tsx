f594771eeb4f99bc9fb77295b6c7f770b272dc12
Julien Cap <4956238+juliencap@users.noreply.github.com>
2026-06-02 22:08:59 +0200

    g33-article-card component

--- src/components/g33-article-card/g33-article-card.tsx
+++ src/components/g33-article-card/g33-article-card.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Prop, h } from '@stencil/core';
 
 @Component({
   tag: 'g33-article-card',
@@ -6,11 +6,50 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class G33ArticleCard {
+  @Prop() postTitle!: string;
+  @Prop() category!: string;
+  @Prop() excerpt?: string;
+  @Prop() imageUrl!: string;
+  @Prop() author?: string;
+  @Prop() publishedAt!: string;
+  @Prop() postUrl: string = '#';
+
   render() {
     return (
-      <Host>
-        <slot></slot>
-      </Host>
+      <article class="post actualite">
+        <div class="featured-image">
+          <a href={this.postUrl}>
+            <img src={this.imageUrl} alt={this.postTitle} />
+          </a>
+        </div>
+
+        <div class="article-content">
+          <div class="above-entry-meta">
+            <span class="cat-links">{this.category}</span>
+          </div>
+
+          <header class="entry-header">
+            <h2 class="entry-title">
+              <a href={this.postUrl}>{this.postTitle}</a>
+            </h2>
+          </header>
+
+          <div class="below-entry-meta">
+            <span class="posted-on">
+              <time class="entry-date">{this.publishedAt}</time>
+            </span>
+
+            <span class="byline">
+              <span class="author vcard">{this.author}</span>
+            </span>
+          </div>
+
+          <div class="entry-content clearfix">
+            <p>{this.excerpt}</p>
+            <g33-button variant="secondary">Lire la suite</g33-button>
+          </div>
+        </div>
+      </article>
     );
   }
 }
