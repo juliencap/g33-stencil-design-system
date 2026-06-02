a25fee4131045c5c5d27f0dc0543b6611bb269a6
Julien Cap <4956238+juliencap@users.noreply.github.com>
2026-06-02 23:02:50 +0200

    g33-hero-banner component

---
+++ src/components/g33-hero-banner/g33-hero-banner.tsx
@@ -0,0 +1,37 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'g33-hero-banner',
+  styleUrl: 'g33-hero-banner.css',
+  shadow: true,
+})
+export class G33HeroBanner {
+  @Prop() titleText!: string;
+  @Prop() subtitle?: string;
+  @Prop() imageUrl?: string;
+
+  @Prop() ctaLabel?: string;
+  @Prop() ctaUrl?: string;
+
+  render() {
+    return (
+      <section class="hero-banner">
+        {this.imageUrl && <img class="hero-banner__image" src={this.imageUrl} alt="" />}
+
+        <div class="hero-banner__overlay"></div>
+
+        <div class="hero-banner__content">
+          <h1>{this.titleText}</h1>
+
+          {this.subtitle && <p>{this.subtitle}</p>}
+
+          {this.ctaUrl && this.ctaLabel && (
+            <a href={this.ctaUrl}>
+              <g33-button variant="ghost">{this.ctaLabel}</g33-button>
+            </a>
+          )}
+        </div>
+      </section>
+    );
+  }
+}
